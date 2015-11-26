47f2901e53482a70cc6f4633b7868da6a292cdb2
Le Minh Pham Hoang <idoraemon0121@gmail.com>
2015-11-27 02:41:44 +0700

    27112015_UPDATE_2

--- public/javascripts/execute.js
+++ public/javascripts/execute.js
@@ -8,12 +8,46 @@ function mod(a, b) {
     return ((a % b) + b) % b;
 }
 
+function gcd(a, b) {
+    if (!b) {
+        return a;
+    }
+    return gcd(b, a % b);
+}
+
 function decrypt_01() {
-    var ENCRYPTED = document.getElementById("code1").value.split('');
-    console.log(ENCRYPTED);
-    var DECRYPTED = '';
+    ENCRYPTED = document.getElementById("code1").value.split('');
+    DECRYPTED = '';
     ENCRYPTED.forEach(function(letter) {
         DECRYPTED += ALPHABET[mod(3 * (ALPHABET.indexOf(letter) - 2), 26)];
     });
     document.getElementById("result1").value = DECRYPTED;
+}
+
+function decrypt_02() {
+    NAME = document.getElementById("code2").value.replace(' ','').split('');
+    ENCRYPTED = '';
+    NAME.forEach(function(letter) {
+        if (letter == letter.toUpperCase()) {
+            ENCRYPTED += ALPHABET[mod(ALPHABET.indexOf(letter) + 7, 26)];
+        } else {
+            ENCRYPTED += LOWER_ALPHABET[mod(LOWER_ALPHABET.indexOf(letter) + 7, 26)];
+        }
+    });
+    document.getElementById("result2").value = ENCRYPTED;
+}
+
+function decrypt_03() {
+    a = parseInt(document.getElementById("param1").value);
+    b = parseInt(document.getElementById("param2").value);
+    CODE = document.getElementById("code3").value.replace(' ','').split('');
+    ENCRYPTED = '';
+    CODE.forEach(function (letter) {
+       if (letter == letter.toUpperCase()) {
+           ENCRYPTED += ALPHABET[mod(ALPHABET.indexOf(letter) * a + b, 26)];
+       } else {
+            ENCRYPTED += LOWER_ALPHABET[mod(LOWER_ALPHABET.indexOf(letter) * a + b, 26)];
+       }
+    });
+    document.getElementById("result3").value = ENCRYPTED;
 }
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'My Playground', CODE: 'NYSYXDSYAXDKYALMWVKYADWDPROPYSNYSYXDKYACZM'});
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Just 4 Fun' });
 });
 
 module.exports = router;
