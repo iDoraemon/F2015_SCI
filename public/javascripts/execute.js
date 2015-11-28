79cdf5db77aaf993d898a78eabcba2050ecd6328
Le Minh Pham Hoang <idoraemon0121@gmail.com>
2015-11-28 18:17:51 +0700

    28112015_UPDATE_3

--- public/javascripts/execute.js
+++ public/javascripts/execute.js
@@ -16,8 +16,8 @@ function gcd(a, b) {
 }
 
 function func01() {
-    ENCRYPTED = document.getElementById("code1").value.split('');
-    DECRYPTED = '';
+    var ENCRYPTED = document.getElementById("code1").value.split('');
+    var DECRYPTED = '';
     ENCRYPTED.forEach(function(letter) {
         DECRYPTED += ALPHABET[mod(3 * (ALPHABET.indexOf(letter) - 2), 26)];
     });
@@ -25,8 +25,8 @@ function func01() {
 }
 
 function func02() {
-    NAME = document.getElementById("code2").value.replace(' ','').split('');
-    ENCRYPTED = '';
+    var NAME = document.getElementById("code2").value.replace(' ','').split('');
+    var ENCRYPTED = '';
     NAME.forEach(function(letter) {
         if (letter == letter.toUpperCase()) {
             ENCRYPTED += ALPHABET[mod(ALPHABET.indexOf(letter) + 7, 26)];
@@ -38,10 +38,10 @@ function func02() {
 }
 
 function func03() {
-    a = parseInt(document.getElementById("param1").value);
-    b = parseInt(document.getElementById("param2").value);
-    CODE = document.getElementById("code3").value.replace(' ','').split('');
-    ENCRYPTED = '';
+    var a = parseInt(document.getElementById("param1").value);
+    var b = parseInt(document.getElementById("param2").value);
+    var CODE = document.getElementById("code3").value.replace(' ','').split('');
+    var ENCRYPTED = '';
     CODE.forEach(function (letter) {
        if (letter == letter.toUpperCase()) {
            ENCRYPTED += ALPHABET[mod(ALPHABET.indexOf(letter) * a + b, 26)];
@@ -53,8 +53,23 @@ function func03() {
 }
 
 function func04() {
+    var r0 = parseInt(document.getElementById("w4_ex1_param1").value);
+    var r1 = parseInt(document.getElementById("w4_ex1_param2").value);
+    if (r0 > r1) {
+        r1 = [r0, r0 = r1][0];
+    }
 
+    var  s = 0, old_s = 1
+        ,t = 1, old_t = 0
+        ,r = r1, old_r = r0;
+
+    while (r != 0) {
+        var quotient = Math.floor(old_r / r);
+        old_r = [r, r = old_r - quotient * r][0];
+
+    }
 }
+
 
 function func05() {
 
