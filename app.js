0987e8829fe5de0602cefe2d39a1071f54423f88
Le Minh Pham Hoang <idoraemon0121@gmail.com>
2015-11-29 20:14:18 +0700

    29112015_UPDATE_4

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var compress = require('compression');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -20,6 +21,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compress());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
