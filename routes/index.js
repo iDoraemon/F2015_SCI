6c99ae22768acbca1312958dc7595164001f09b8
Le Minh Pham Hoang <idoraemon0121@gmail.com>
2016-01-05 15:45:32 +0000

    nothing

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Just 4 Fun' });
+  res.render('index', { title: 'Project Web App Programming' });
 });
 
 module.exports = router;
